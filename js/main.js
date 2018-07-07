2bebcc3a38cffe3add5bfa25473cf0f8b315aaec
Diana Toma <toma.andreeadiana@yahoo.com>
2018-07-07 10:55:05 +0300

    v.1.1

--- js/main.js
+++ js/main.js
@@ -3,11 +3,64 @@ let configCharacter = document.querySelector('.character_details');
 let detailsConfig = document.querySelector('.configured_character_details');
 let storeConfig = JSON.parse(localStorage.getItem('info')) || [];
 
+configCharacter.addEventListener('submit', addCharacterDetails);
+showCharacterDetails(storeConfig, detailsConfig);
+showInfo();
+
+// get references for my actions and myPet
+let submitBtn = document.querySelector('button[type="submit"]');
+let feedBtn = document.getElementById('feed');
+let playBtn = document.getElementById('play');
+let sleepBtn = document.getElementById('sleep');
+let myPet = createPet(10, 20, 30);
+let myPetMessages = {
+    hungerMessage: {
+        info: 'I\'m fullfill!!!',
+        warning: 'Feed me human!'
+    },
+    hapinessMessage: {
+        info: 'Happy!!! I don\'t want to play now!',
+        warning: 'I\'m so sad! PLay with me!'
+    },
+    restMessage: {
+        info: `ZZZZZZZZZZZZZZZZZZZZZ!`,
+        warning: `I want to sleep!`,
+    }
+}
+
+window.addEventListener("load", function () {
+    getMyPetInfo();
+    changeProgressUI(food, myPet.hunger.progress);
+    changeProgressUI(joy, myPet.hapiness.progress);
+    changeProgressUI(rest, myPet.rest.progress);
+    isPetAlive();
+});
+feedBtn.addEventListener('click', function () {
+    myPet.increaseState(food, myPet.hunger.progress, 'hunger');
+});
+playBtn.addEventListener('click', function () {
+    myPet.increaseState(joy, myPet.hapiness.progress, 'hapiness');
+});
+sleepBtn.addEventListener('click', function () {
+    myPet.increaseState(rest, myPet.rest.progress, 'rest');
+});
+idInterval = setInterval(function () {
+    myPet.decreaseState(food, myPet.hunger.progress, 'hunger');
+    myPet.decreaseState(joy, myPet.hapiness.progress, 'hapiness');
+    myPet.decreaseState(rest, myPet.rest.progress, 'rest');
+    isPetAlive();
+}, 10000);
+
+let pictureInput = document.getElementById('chooseImg');
+
+pictureInput.addEventListener('change', showFile);
+pictureInput.addEventListener('change', displayImage);
+
 function showInfo() {
     if (storeConfig.length === 0) {
-        $(configCharacter).show();
+        configCharacter.classList.remove('hidden');
     } else {
-        $(configCharacter).hide();
+        configCharacter.classList.add('hidden');
     }
 }
 function addCharacterDetails(e) {
@@ -24,20 +77,8 @@ function addCharacterDetails(e) {
     localStorage.setItem('info', JSON.stringify(storeConfig));
     showCharacterDetails(storeConfig, detailsConfig);
     this.reset();
-    $(this).hide();
+    submitBtn.classList.add('hidden');
     showInfo();
-}
-/** invoke this function to hide the form after is completed and reset */
-function $(selector) {
-    let resultObject = {
-        hide: function () {
-            selector.style.display = "none";
-        },
-        show: function () {
-            selector.style.display = "block";
-        }
-    };
-    return resultObject;
 }
 function showCharacterDetails(details = [], detailsList) {
     detailsList.innerHTML = details.map((detail) => {
@@ -53,8 +94,8 @@ function showCharacterDetails(details = [], detailsList) {
 }
 function displayImage() {
     let create_pet = document.querySelector('.character_details');
-    let file = document.getElementById("chooseImg");
-    let firstFile = file.files[0];
+    let inputFile = document.getElementById("chooseImg");
+    let firstFile = inputFile.files[0];
     let reader = new FileReader();
     reader.onload = function (e) {
         let image = document.createElement("img");
@@ -64,82 +105,41 @@ function displayImage() {
     }
     reader.readAsDataURL(firstFile);
 }
-showInfo();
-configCharacter.addEventListener('submit', addCharacterDetails);
-showCharacterDetails(storeConfig, detailsConfig);
-
-
-/** create the html for warnings */
-let myPet = {
-    foodList: ['pizza', 'pasta', 'shrimp'],
-    hunger: {
-        progress: 10,
-        step: 10,
-        messages: {
-            info: 'I\'m fullfill!!!',
-            warning: 'Feed me human!'
+function createPet(progressHunger, progressHapiness, progressRest, awake = false, step = 10, age = 0) {
+    return {
+        hunger: {
+            progress: progressHunger,
+            step
+        },
+        hapiness: {
+            progress: progressHapiness,
+            step
+        },
+        awake,
+        rest: {
+            progress: progressRest,
+            step
+        },
+        age,
+        isAlive: function () {
+            if ((this.rest.progress === 10) || (this.hunger.progress === 10) || (this.hapiness.progress === 10) || this.age === 15) {
+                displayMessages(`SORRY, YOUR PET IS DEAD!`, 'warning');
+            } else {
+                return true;
+            };
+        },
+        increaseState: function (idProgress, value, type) {
+            increaseLevel(value, type);
+            changeProgressUI(idProgress, myPet[type].progress);
+            saveMyPetInfo();
+        },
+        decreaseState: function (idProgress, value, type) {
+            decreaseLevel(type);
+            changeProgressUI(idProgress, myPet[type].progress);
+            saveMyPetInfo();
         }
-    },
-    hapiness: {
-        progress: 20,
-        step: 10,
-        messages: {
-            info: 'Happy!!! I don\'t want to play now!',
-            warning: 'I\'m so sad! PLay with me!'
-        }
-    },
-    awake: false,
-    rest: {
-        progress: 30,
-        step: 10,
-        messages: {
-            info: `ZZZZZZZZZZZZZZZZZZZZZ!`,
-            warning: `I want to sleep!`,
-
-        }
-    },
-    age: {
-        progress: 0,
-        oldData: Math.round(new Date().getTime() / 1000)
-    },
-    // cu astea inca nu fac nimic asa ca le-am comentat pt moment
-    // feed: function () {
-    //     if (this.hunger.progress < 35) {
-    //         this.getSad();
-    //     }
-    // },
-    // getSad() {
-    //     if (this.hapiness.progress > 0) {
-    //         return this.HAPINESS > PROGRESS--;
-    //     }
-    // },
-    // sleep: function () {
-    //     if (this.awake) {
-    //         this.awake = false;
-    //         this.getSad();
-    //     }
-    // },
-    // play: function () {
-    //     if (this.hapiness.progress > 20) {
-    //         this.hapiness.progress--;
-    //     }
-    // },
-    
-    isAlive: function () {
-        if ((this.rest.progress === 0) || (this.hunger.progress === 0) || (this.hapiness.progress === 0) || this.AGE === 15) {
-            displayMessages(`SORRY, YOUR PET IS DEAD!`, 'warning');
-        } else {
-            return true;
-        };
     }
 }
-
-
-// get references for my actions
-let feedBtn = document.getElementById('feed');
-let playBtn = document.getElementById('play');
-let sleepBtn = document.getElementById('sleep');
-
 function displayMessages(textMessage, typeOfMessage) {
     let wrapper_messages = document.querySelector(".wrapper_messages");
     let container_messages = document.createElement('div');
@@ -157,42 +157,35 @@ function displayMessages(textMessage, typeOfMessage) {
     closeBtn.onclick = function () {
         container_messages.parentNode.removeChild(container_messages);
     }
-
     if (typeOfMessage === 'warning') {
-        container_messages.style.backgroundColor = '#dc3545';
+        container_messages.classList.add('bg--warning');
     }
     else if
     (typeOfMessage === 'info') {
-        container_messages.style.backgroundColor = '#e85cde';
+        container_messages.classList.add('bg--info');
     } else {
-        container_messages.style.backgroundColor = '#6c757d';
+        container_messages.classList.add('bg--default');
     }
-    //hide the message after 3 seconds
+    //remove the message after 3 seconds
     setTimeout(function () {
-        container_messages.style.display = 'none';
+        container_messages.parentNode.removeChild(container_messages);
     }, 3000); //  
-}
-
-function increaseState(idProgress, value, type) {
-    increaseLevel(value, type);
-    changeColor(idProgress, myPet[type].progress);
-    saveMyPetInfo();
 }
 function increaseLevel(value, type) {
     if (value > 20 && value < 35) {
-        displayMessages(myPet[type].messages.warning, 'warning');
+        displayMessages(myPet[type].warning, 'warning');
     } else if ((value > 90) && (value <= 100)) {
-        displayMessages(myPet[type].messages.info, 'info')
+        displayMessages(myPet[type].info, 'info')
     } else if (value > 100) {
         value = 100;
-        displayMessages(myPet[type].messages.info, 'info')
+        displayMessages(myPet[type].info, 'info')
     }
     myPet[type].progress += myPet[type].step;
     if (myPet[type].progress > 100) {
         myPet[type].progress = 100;
     }
 }
-function changeColor(idProgress, value) {
+function changeProgressUI(idProgress, value) {
     if (value < 40) {
         idProgress.className = "progress state_danger";
     }
@@ -224,44 +217,28 @@ function getMyPetInfo() {
         myPet.age.progress = localStor.age.progress;
     }
 };
-function initUI(myPetObj, idProgress, type) {
-    changeColor(idProgress, myPetObj[type].progress);
-}
-idInterval = setInterval(function () {
-    decreaseState(food, myPet.hunger.progress, 'hunger');
-    decreaseState(joy, myPet.hapiness.progress, 'hapiness');
-    decreaseState(rest, myPet.rest.progress, 'rest');
-    isPetAlive();
-}, 500000);
-
+// function initUI(myPetObj, idProgress, type) {
+//     changeProgressUI(idProgress, myPetObj[type].progress);
+// }
 function isPetAlive() {
     if (!myPet.isAlive()) {
         clearInterval(idInterval);
     }
 }
-function decreaseState(idProgress, value, type) {
-    saveMyPetInfo();
-    decreaseLevel(type);
-    changeColor(idProgress, myPet[type].progress);
-}
 function decreaseLevel(type) {
     myPet[type].progress -= myPet[type].step;
 }
-feedBtn.addEventListener('click', function () {
-    increaseState(food, myPet.hunger.progress, 'hunger');
-});
-playBtn.addEventListener('click', function () {
-    increaseState(joy, myPet.hapiness.progress, 'hapiness');
-});
-sleepBtn.addEventListener('click', function () {
-    increaseState(rest, myPet.rest.progress, 'rest');
-});
+function showFile(e) {
+    let pictureLabel = inputFile.nextElementSibling;
+    let labelVal = pictureLabel.innerHTML;
+    let fileName = '';
+    if (this.files && this.files.length > 1)
+        fileName = (this.getAttribute('data-multiple-caption') || '').replace('{count}', this.files.length);
+    else
+        fileName = e.target.value.split('\\').pop();
 
-window.addEventListener("load", function () {
-    getMyPetInfo();
-    initUI(myPet, food, 'hunger');
-    initUI(myPet, joy, 'hapiness');
-    initUI(myPet, rest, 'rest');
-    isPetAlive();
-});
-
+    if (fileName)
+        pictureLabel.querySelector('span').innerHTML = fileName;
+    else
+        pictureLabel.innerHTML = labelVal;
+}
